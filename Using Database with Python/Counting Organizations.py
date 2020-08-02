8e0cdb9edfc1a5054e478cca0131d8d5d4273961
Frank DU <frankdu0207@gmail.com>
2020-08-02 18:36:26 +0800

    initial commit

--- Using Database with Python/Counting Organizations.py
+++ Using Database with Python/Counting Organizations.py
@@ -1,3 +1,6 @@
+# This application will read the mailbox data (mbox.txt) and count the number of email messages per organization
+# (i.e. domain name of the email address) using a database with the following schema to maintain the counts.
+
 import sqlite3
 import re
 
